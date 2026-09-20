133898d6467c479403d47346ed30bd54af608ca0
git config --global user.name "kumar-sudhakar <git config --global user.name "kumar-sudhakar" git config --global user.email "your-github-email@example.com>
2026-09-20 16:43:05 +0530

    Mount all components and pages in App and add CustomCursor and ParticleBackground components

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import ParticlesBackground from "./components/ParticlesBackground";
+import CustomCursor from "./components/CustomCursor";
+import Navbar from "./components/Navbar";
+import Home from "./sections/Home";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Experience from "./sections/Experience";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
-const App = () => {
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="relative gradient text-white">
+      <ParticlesBackground />
+      <CustomCursor />
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
-
-export default App
